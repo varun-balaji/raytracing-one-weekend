49449a8aad79ee8f656bd07cbfc052703b04368c
varun-balaji <varunbalaji40@gmail.com>
2024-11-05 19:33:13 +0530

    Add a sphere

--- main.cc
+++ main.cc
@@ -2,7 +2,24 @@
 #include "ray.h"
 #include <iostream>
 
+bool hit_sphere(const point3 &sphere_center, double sphere_radius,
+                const ray &incident_ray) {
+  vec3 CQ = sphere_center - incident_ray.origin();
+  auto a = dot(incident_ray.direction(), incident_ray.direction());
+  auto b = -2.0 * dot(incident_ray.direction(), CQ);
+  auto c = dot(CQ, CQ) - sphere_radius * sphere_radius;
+  auto discriminant = b * b - 4 * a * c;
+
+  // Cannot simplify further since dot product isn't associative
+
+  return (discriminant >= 0);
+}
+
 color ray_color(const ray &r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -19,10 +36,13 @@ int main() {
   }
 
   // Camera
-  auto focal_length = 1.0;    // Distance from viewport to camera
-  auto viewport_height = 2.0; // NOTE: Arbitary value? Is this not pixels?
+  auto focal_length = 1.0; // Distance from viewport to camera
+  auto viewport_height = 2.0;
   auto viewport_width = (viewport_height * image_width) / image_height;
   auto camera_center = point3(0, 0, 0); // Camera at origin
+
+  // Viewport dimensions are mapped to the image dimensions. This is where the
+  // scalar mapping of pixels to 3D coordinate unit dimensions happens.
 
   // Viewport edge vectors
   auto viewport_x = vec3(viewport_width, 0, 0);
